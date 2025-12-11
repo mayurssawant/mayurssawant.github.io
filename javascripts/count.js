d4e26e458aa40b35153aa485129d81e943adb300
Sat-Chit-Anand <mayursawantd1@gmail.com>
2025-12-11 10:17:06 -0500

    Deployed b40dfa0 with MkDocs version: 1.6.1

---
+++ javascripts/count.js
@@ -0,0 +1,7 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = '//gc.zgo.at/count.js';
+    script.async = true;
+    script.dataset.goatcounter = 'https://mayurssawant.goatcounter.com/count';
+    document.head.appendChild(script);
+})();
