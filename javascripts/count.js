340440b4aeba190a991795ab603d5ddddf88598c
Mayur <mayursawantd1@gmail.com>
2026-06-09 13:08:45 -0400

    Deploy homepage title update

--- javascripts/count.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = '//gc.zgo.at/count.js';
-    script.async = true;
-    script.dataset.goatcounter = 'https://mayurssawant.goatcounter.com/count';
-    document.head.appendChild(script);
-})();
